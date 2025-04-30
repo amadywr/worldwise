b1a107062c44e41ec6929b5f41a9b145ca992212
Ahmad Yawari <k_han21@yahoo.com>
2025-04-30 18:52:05 +1000

    minor bug fix for netlify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import Logo from './Logo'
 import AppNav from './AppNav'
-import styles from './sidebar.module.css'
+import styles from './Sidebar.module.css'
 import { Outlet } from 'react-router-dom'
 
 function Sidebar() {
